55e4027a514416356e592d5a78fb27533380f0d2
Ric Evans <emejqz@gmail.com>
2021-05-21 12:26:21 -0500

    update setup.py (dogfooding)

--- setup.py
+++ setup.py
@@ -6,22 +6,17 @@ from setuptools import setup  # type: ignore[import]
 
 from wipac_dev_tools import SetupShop
 
-setupshop = SetupShop(
+shop = SetupShop(
     "wipac_dev_tools",
     os.path.abspath(os.path.dirname(__file__)),
     ((3, 6), (3, 8)),
     "WIPAC Python Development Tools",
+    allow_git_urls=False,
 )
 
 setup(
-    name=setupshop.name,
-    version=setupshop._version,
-    **setupshop.author_kwargs,
-    **setupshop.description_kwargs,
+    **shop.get_kwargs(other_classifiers=["License :: OSI Approved :: MIT License"]),
     url="https://github.com/WIPACrepo/wipac-dev-tools",
     license="MIT",
-    classifiers=setupshop.get_classifiers(["License :: OSI Approved :: MIT License"]),
-    packages=setupshop.get_packages(),
-    install_requires=setupshop.get_install_requires(allow_git_urls=False),
-    package_data={setupshop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
+    package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
 )
