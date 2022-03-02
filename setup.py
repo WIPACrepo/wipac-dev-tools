77075b191e5a301e77b3165e6e85975a096037c6
Ric Evans <emejqz@gmail.com>
2022-03-02 16:53:33 -0600

    add `FromFiles` to parse files for version string & assert dir structure

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
 """Setup."""
 
-import os
 
-from setuptools import setup  # type: ignore[import]
-
-from wipac_dev_tools import SetupShop
-
-shop = SetupShop(
-    "wipac_dev_tools",
-    os.path.abspath(os.path.dirname(__file__)),
-    ((3, 6), (3, 8)),
-    "WIPAC Python Development Tools",
-    allow_git_urls=False,
-)
+from setuptools import setup
 
 setup(
-    **shop.get_kwargs(),
-    url="https://github.com/WIPACrepo/wipac-dev-tools",
-    package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
+    # **shop.get_kwargs(),
+    # url="https://github.com/WIPACrepo/wipac-dev-tools",
+    # package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
 )
