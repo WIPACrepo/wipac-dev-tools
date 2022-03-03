4f8d421a922c0170c140e1cd1acc14ed16377fbc
Ric Evans <emejqz@gmail.com>
2022-03-02 18:46:18 -0600

    remove commented-out code

--- setup.py
+++ setup.py
@@ -3,8 +3,4 @@
 
 from setuptools import setup
 
-setup(
-    # **shop.get_kwargs(),
-    # url="https://github.com/WIPACrepo/wipac-dev-tools",
-    # package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
-)
+setup()
