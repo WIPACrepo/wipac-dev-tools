af42b273b97c774e21abd0b6dbedd74e2aaae8e5
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2022-03-07 15:21:06 -0600

    PyPI Readiness [minor] (#22)
    Co-authored-by: github-actions <github-actions@github.com>

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 """Setup."""
 
-import os
 
-from setuptools import setup  # type: ignore[import]
+from setuptools import setup  # type: ignore[import]  # mypy error in GH Action
 
-from wipac_dev_tools import SetupShop
-
-shop = SetupShop(
-    "wipac_dev_tools",
-    os.path.abspath(os.path.dirname(__file__)),
-    ((3, 6), (3, 8)),
-    "WIPAC Python Development Tools",
-    allow_git_urls=False,
-)
-
-setup(
-    **shop.get_kwargs(),
-    url="https://github.com/WIPACrepo/wipac-dev-tools",
-    package_data={shop.name: ["data/www/*", "data/www_templates/*", "py.typed"]},
-)
+setup()
