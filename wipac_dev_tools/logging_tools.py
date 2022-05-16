2d31d647188842220c0d9c14c564dcd7731b95f0
Ric Evans <emejqz@gmail.com>
2022-05-16 12:24:03 -0500

    add `logging_tools.set_level()`

---
+++ wipac_dev_tools/logging_tools.py
@@ -0,0 +1,38 @@
+"""Common tools to supplement/assist the standard logging package."""
+
+import logging
+from typing import List, Optional, Union
+
+
+def set_level(
+    level: str,
+    first_party_loggers: Optional[List[Union[str, logging.Logger]]] = None,
+    third_party_level: str = "WARNING",
+    use_coloredlogs: bool = False,
+) -> None:
+    """Set the level of the root logger, first-party loggers, and third-party loggers."""
+    if not first_party_loggers:
+        first_party_loggers = []
+
+    # root
+    if use_coloredlogs:
+        import coloredlogs  # type: ignore[import]  # pylint: disable=import-outside-toplevel
+
+        coloredlogs.install(level=level)  # root
+    else:
+        logging.getLogger().setLevel(level)
+
+    # first-party
+    for log in first_party_loggers:
+        if isinstance(log, logging.Logger):
+            log.setLevel(level)
+        else:  # str
+            logging.getLogger(log).setLevel(level)
+
+    # third-party
+    for log_name in logging.root.manager.loggerDict:
+        if log_name in first_party_loggers:
+            continue
+        if logging.getLogger(log_name) in first_party_loggers:
+            continue
+        logging.getLogger(log_name).setLevel(third_party_level)
