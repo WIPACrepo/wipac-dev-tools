0d2c36d888705a4898d9aeaece159e51b0dc7582
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2022-09-26 18:53:14 -0500

    `argparse_tools`: Auto-wrap `ArgumentTypeError` If Needed (#43)
    Co-authored-by: github-actions <github-actions@github.com>

--- wipac_dev_tools/argparse_tools.py
+++ wipac_dev_tools/argparse_tools.py
@@ -1,5 +1,6 @@
 """Simple tools used with argparse."""
 
+import argparse
 from pathlib import Path
 from typing import Optional, TypeVar
 
@@ -7,9 +8,22 @@ T = TypeVar("T")
 
 
 def validate_arg(val: T, test: bool, exc: Exception) -> T:
-    """Validation `val` by checking `test` and raise `exc` if that is falsy."""
+    """Validation `val` by checking `test` and raise `exc` if that is falsy.
+
+    If the passed exception is not an instance of `ArgumentTypeError`,
+    then an `ArgumentTypeError` instance is raised, like so:
+        `ArgumentTypeError(f"{repr(exc)} [{val}]")`
+    """
     if test:
         return val
+
+    # The argument to type can be any callable that accepts a single string.
+    # If the function raises ArgumentTypeError, TypeError, or ValueError,
+    # the exception is caught and a nicely formatted error message is displayed.
+    # No other exception types are handled.
+    if not isinstance(exc, argparse.ArgumentTypeError):
+        raise argparse.ArgumentTypeError(f"{repr(exc)} [{val}]")
+
     raise exc
 
 
