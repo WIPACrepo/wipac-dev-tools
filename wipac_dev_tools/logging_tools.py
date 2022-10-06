3607bff5699aa2a27d1c39996fa159f16d4331d4
Ric Evans <emejqz@gmail.com>
2022-10-06 10:59:28 -0500

    let's pump the brakes, this shouldn't be so complicated

--- wipac_dev_tools/logging_tools.py
+++ wipac_dev_tools/logging_tools.py
@@ -96,12 +96,6 @@ def _set_and_share(log_name: str, level: LoggerLevel, text: str) -> None:
     logging.getLogger().info(f"{text} Logger: '{log_name}' ({level})")
 
 
-def _get_all_ancestors(dotted: str) -> Iterator[str]:
-    parts = dotted.split(".")
-    for i in range(len(parts)):
-        yield ".".join(parts[: i + 1])  # i=1 gets first 2 elements, dotted
-
-
 def set_level(
     level: LoggerLevel,
     first_party_loggers: Union[
@@ -143,15 +137,11 @@ def set_level(
                 f"a list of Logger instances or names: {first_party_loggers}"
             )
 
-    third_parties = list(logging.root.manager.loggerDict) + _to_list(
-        future_third_parties
-    )
-
     return _set_level(
         level.upper(),  # type: ignore
         first_parties,
         third_party_level.upper(),  # type: ignore
-        third_parties,
+        list(logging.root.manager.loggerDict) + _to_list(future_third_parties),
         use_coloredlogs,
     )
 
@@ -163,10 +153,6 @@ def _set_level(
     third_parties: List[str],
     use_coloredlogs: bool,
 ) -> None:
-    third_parties = list(set(third_parties))
-    first_parties = list(set(first_parties))
-    all_known_loggers = {}
-
     # root
     if use_coloredlogs:
         try:
@@ -183,27 +169,12 @@ def _set_level(
         logging.getLogger().setLevel(first_party_level)
     logging.getLogger().info(f"Root Logger: '' ({first_party_level})")
 
-    # get every known logger
-    for log_name in third_parties + first_parties:
-        # Add every ancestor
-        # # In theory we might be doing WAY more sets than necessary,
-        # # but in reality logger hierarchy chains aren't super long.
-        # # IOW, we could just set the first ancestor, then the 1st party
-        # # but the extra logging can be useful for debugging.
-        for ancestor in _get_all_ancestors(log_name):
-            all_known_loggers[ancestor] = False
-
-    # promote first-party loggers (& their descendants)
-    for log_name in first_parties:
-        for key in all_known_loggers:
-            if key == log_name or key.startswith(log_name + "."):
-                all_known_loggers[key] = True
-
     # third-party
-    for log_name, is_first_party in sorted(all_known_loggers.items()):
-        if not is_first_party:
-            _set_and_share(log_name, third_party_level, "Third-Party")
-    # first-party (set these last to override the setting inherited via an ancestor)
-    for log_name, is_first_party in sorted(all_known_loggers.items()):
-        if is_first_party:
-            _set_and_share(log_name, first_party_level, "First-Party")
+    for log_name in sorted(set(third_parties)):
+        ancestor = log_name.split(".", maxsplit=1)[0]
+        if ancestor not in first_parties:
+            _set_and_share(ancestor, third_party_level, "Third-Party")
+
+    # first-party
+    for log_name in sorted(set(first_parties)):
+        _set_and_share(log_name, first_party_level, "First-Party")
