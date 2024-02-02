c48fb30d0b8703b1999376c236c0f8e509603b78
Ric Evans <ric@evans-jacquez.com>
2024-02-02 15:11:34 -0600

    type fix

--- tests/semvar_parser_test.py
+++ tests/semvar_parser_test.py
@@ -1,4 +1,3 @@
-
 """semvar_parser_test.py."""
 
 
@@ -15,22 +14,22 @@ def test_000() -> None:
         major=3,
         semvar_range=">=3.5.1, <3.9",
         # max_minor=99,
-    ) == ["3.6", "3.7", "3.8"]
+    ) == [(3, 6), (3, 7), (3, 8)]
 
     assert semvar_parser_tools.list_all_majmin_versions(
         major=3,
         semvar_range=">=3.5.1",
         max_minor=8,
-    ) == ["3.6", "3.7", "3.8"]
+    ) == [(3, 6), (3, 7), (3, 8)]
 
     assert semvar_parser_tools.list_all_majmin_versions(
         major=3,
         semvar_range=">=3,<3.6,!=3.3",
         # max_minor=99,
-    ) == ["3.0", "3.1", "3.2", "3.4", "3.5"]
+    ) == [(3, 0), (3, 1), (3, 2), (3, 4), (3, 5)]
 
-    assert semvar_parser_tools.list_all_majmin_versions(
+    assert not semvar_parser_tools.list_all_majmin_versions(
         major=2,
         semvar_range=">=3.5.1",
         # max_minor=99,
-    ) == []
+    )
