20a6f278ae107e017bd2c02df9efafbe28059d31
ric-evans <emejqz@gmail.com>
2024-05-31 17:20:10 -0400

    re-add `__version__`, now with `importlib.metadata.version`

--- wipac_dev_tools/__init__.py
+++ wipac_dev_tools/__init__.py
@@ -1,5 +1,7 @@
 """Init."""
 
+import importlib
+
 from . import argparse_tools, data_safety_tools, logging_tools
 from .enviro_tools import from_environment, from_environment_as_dataclass  # noqa
 from .setup_tools import SetupShop  # noqa
@@ -14,3 +16,19 @@ __all__ = [
     "argparse_tools",
     "data_safety_tools",
 ]
+
+# version is a human-readable version number.
+__version__ = importlib.metadata.version(__package__ or __name__)
+
+
+# version_info is a four-tuple for programmatic comparison. The first
+# three numbers are the components of the version number. The fourth
+# is zero for an official release, positive for a development branch,
+# or negative for a release candidate or beta (after the base version
+# number has been incremented)
+version_info = (
+    int(__version__.split(".")[0]),
+    int(__version__.split(".")[1]),
+    int(__version__.split(".")[2]),
+    0,
+)
