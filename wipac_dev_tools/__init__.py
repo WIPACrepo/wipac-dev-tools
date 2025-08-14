91247c5c179594b1ef30e40c69f6ea31b5bf0323
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2025-08-14 15:10:40 -0500

    Update CI [bump] (#131)
    Co-authored-by: github-actions <github-actions@github.com>

--- wipac_dev_tools/__init__.py
+++ wipac_dev_tools/__init__.py
@@ -18,5 +18,5 @@ __all__ = [
     "mongo_jsonschema_tools",  # not imported above b/c module has optional dependencies
 ]
 
-# NOTE: `__version__` is not defined as this package is built using 'setuptools-scm' --
-#   use `importlib.metadata.version(...)` if you need to access version info at runtime
+# NOTE: `__version__` is not defined because this package is built using 'setuptools-scm' --
+#   use `importlib.metadata.version(...)` if you need to access version info at runtime.
