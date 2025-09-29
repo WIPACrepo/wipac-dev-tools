3d1305174d18c31fed2f0c17eb5f40140623d7cf
ric-evans <emejqz@gmail.com>
2025-09-29 14:35:54 -0500

    classify

--- wipac_dev_tools/container_registry_tools.py
+++ wipac_dev_tools/container_registry_tools.py
@@ -26,93 +26,102 @@ class ImageNotFoundException(Exception):
 ########################################################################################
 
 
-def get_cvmfs_image_path(
-    cvmfs_images_dir: Path,
-    image_name: str,
-    tag: str,
-    check_exists: bool = False,
-) -> Path:
-    """Get the image path for 'tag' (optionally, check if it exists)."""
-    dpath = cvmfs_images_dir / f"{image_name}:{tag}"
+class ImageCVMFS:
+    """Tools for working with CVMFS images."""
 
-    # optional guardrail
-    if check_exists and not dpath.exists():
-        raise ImageNotFoundException(dpath)
+    def __init__(self, cvmfs_images_dir: Path):
+        # ex: /cvmfs/icecube.opensciencegrid.org/containers/realtime/
+        self.cvmfs_images_dir = cvmfs_images_dir
 
-    return dpath
+    def get_image_path(
+        self,
+        image_name: str,
+        tag: str,
+        check_exists: bool = False,
+    ) -> Path:
+        """Get the image path for 'tag' (optionally, check if it exists)."""
 
+        # ex: /cvmfs/icecube.opensciencegrid.org/containers/realtime/skymap_scanner:v4.5.62
+        dpath = self.cvmfs_images_dir / f"{image_name}:{tag}"
 
-def iter_x_y_z_cvmfs_tags(cvmfs_images_dir: Path, image_name: str) -> Iterable[str]:
-    """Iterate over all 'X.Y.Z' skymap scanner tags on CVMFS, youngest to oldest."""
+        # optional guardrail
+        if check_exists and not dpath.exists():
+            raise ImageNotFoundException(dpath)
 
-    # grab all container dirs -- ordered by age
-    # ex: /cvmfs/icecube.opensciencegrid.org/containers/realtime/skymap_scanner:*
-    cvmfs_tags = sorted(
-        cvmfs_images_dir.glob(f"{image_name}:*"),
-        key=lambda x: x.stat().st_mtime,  # filesystem modification time
-        reverse=True,  # newest -> oldest
-    )
+        return dpath
 
-    # yield only 'X.Y.Z' tags
-    for p in cvmfs_tags:
-        try:
-            tag = p.name.split(":", maxsplit=1)[1]
-        except IndexError:
-            continue
-        if not RE_VERSION_X_Y_Z.fullmatch(tag):
-            continue
-        # tag is a full 'X.Y.Z' tag
-        yield tag
+    def iter_x_y_z_tags(self, image_name: str) -> Iterable[str]:
+        """Iterate over all 'X.Y.Z' skymap scanner tags on CVMFS, youngest to oldest."""
 
-
-def resolve_tag_on_cvmfs(
-    cvmfs_images_dir: Path,
-    image_name: str,
-    docker_tag: str,
-) -> str:
-    """Get the 'X.Y.Z' tag on CVMFS corresponding to `docker_tag`.
-
-    Examples:
-        3.4.5     ->  3.4.5
-        3.1       ->  3.1.5 (forever)
-        3         ->  3.3.5 (on 2023/03/08)
-        latest    ->  3.4.2 (on 2023/03/15)
-        test-foo  ->  test-foo
-        typO_t4g  ->  `ImageNotFoundException`
-    """
-    LOGGER.info(f"checking tag exists on cvmfs: {docker_tag}")
-
-    # step 0: prep tag
-    try:
-        docker_tag = strip_v_prefix(docker_tag)
-    except ValueError as e:
-        raise ImageNotFoundException(docker_tag) from e
-
-    # step 1: does the tag simply exist on cvmfs?
-    try:
-        _path = get_cvmfs_image_path(
-            cvmfs_images_dir,
-            image_name,
-            docker_tag,
-            check_exists=True,
+        # grab all container dirs -- ordered by age
+        # ex: /cvmfs/icecube.opensciencegrid.org/containers/realtime/skymap_scanner:*
+        cvmfs_tags = sorted(
+            self.cvmfs_images_dir.glob(f"{image_name}:*"),
+            key=lambda x: x.stat().st_mtime,  # filesystem modification time
+            reverse=True,  # newest -> oldest
         )
-        LOGGER.debug(f"tag exists on cvmfs: {_path}")
-        return docker_tag
-    except ImageNotFoundException:
-        pass
 
-    # step 2: was the tag a non-specific tag (like 'latest', 'v4.1', 'v4', etc.)
-    # -- case 1: user gave 'latest'
-    if docker_tag == "latest":
-        for tag in iter_x_y_z_cvmfs_tags(cvmfs_images_dir, image_name):
-            LOGGER.debug(f"resolved 'latest' to youngest X.Y.Z tag: {tag}")
-            return tag
-    # -- case 2: user gave an non-specific semver tag (like 'v4.1', 'v4', etc.)
-    elif RE_VERSION_X_Y.fullmatch(docker_tag) or RE_VERSION_X.fullmatch(docker_tag):
-        for tag in iter_x_y_z_cvmfs_tags(cvmfs_images_dir, image_name):
-            if tag.startswith(docker_tag + "."):  # ex: '3.1.4' startswith '3.1.'
-                LOGGER.debug(f"resolved '{docker_tag}' to '{tag}'")
-                return tag
+        # yield only 'X.Y.Z' tags
+        for p in cvmfs_tags:
+            try:
+                tag = p.name.split(":", maxsplit=1)[1]
+            except IndexError:
+                continue
+            if not RE_VERSION_X_Y_Z.fullmatch(tag):
+                continue
+            # tag is a full 'X.Y.Z' tag
+            yield tag
 
-    # fall-through
-    raise ImageNotFoundException(docker_tag)
+    def resolve_tag(
+        self,
+        image_name: str,
+        source_tag: str,
+    ) -> str:
+        """Get the 'X.Y.Z' tag on CVMFS corresponding to `source_tag`.
+
+        Examples:
+            3.4.5     ->  3.4.5
+            3.1       ->  3.1.5 (forever)
+            3         ->  3.3.5 (on 2023/03/08)
+            latest    ->  3.4.2 (on 2023/03/15)
+            test-foo  ->  test-foo
+            typO_t4g  ->  `ImageNotFoundException`
+        """
+        LOGGER.info(f"checking tag exists on cvmfs: {source_tag}")
+
+        # step 0: prep tag
+        try:
+            source_tag = strip_v_prefix(source_tag)
+        except ValueError as e:
+            raise ImageNotFoundException(source_tag) from e
+
+        # step 1: does the tag simply exist on cvmfs?
+        try:
+            _path = self.get_image_path(image_name, source_tag, check_exists=True)
+            LOGGER.debug(f"tag exists on cvmfs: {_path}")
+            return source_tag
+        except ImageNotFoundException:
+            pass
+
+        # step 2: was the tag a non-specific tag (like 'latest', 'v4.1', 'v4', etc.)
+        # -- case 1: user gave 'latest'
+        if source_tag == "latest":
+            for source_tag in self.iter_x_y_z_tags(image_name):
+                LOGGER.debug(f"resolved 'latest' to youngest X.Y.Z tag: {source_tag}")
+                return source_tag
+        # -- case 2: user gave an non-specific semver tag (like 'v4.1', 'v4', etc.)
+        elif RE_VERSION_X_Y.fullmatch(source_tag) or RE_VERSION_X.fullmatch(source_tag):
+            for source_tag in self.iter_x_y_z_tags(image_name):
+                if source_tag.startswith(
+                    source_tag + "."
+                ):  # ex: '3.1.4' startswith '3.1.'
+                    LOGGER.debug(f"resolved '{source_tag}' to '{source_tag}'")
+                    return source_tag
+
+        # fall-through
+        raise ImageNotFoundException(source_tag)
+
+
+########################################################################################
+# REGISTRY: DOCKERHUB
+########################################################################################
